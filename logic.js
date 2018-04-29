bf31ddb0238c6d7081b0ee6dae4e99777f28baa7
Laridae <christoffer14@gmail.com>
2018-04-29 14:58:21 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -5,5 +5,4 @@ function start_question(){
 function show_next_question(d) {
  var currentQ = document.getElementById("question" + d).setAttribute("class", "question inactive");
  var nextQ = document.getElementById("question" + (d+1)).setAttribute("class", "question active");
- currentQ set attribute
 }
