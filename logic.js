7491d844d7766dfca3b776aec80aae2fd3f435de
Laridae <christoffer14@gmail.com>
2018-04-29 14:45:39 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -4,6 +4,8 @@ var tick = 1
 
 function show_next_question(d) {
  var currentQ = document.getElementById("question" + d);
+ var nextQ = document.getElementById("fråga" + (d+1));
+ 
  hide div.d; 
  show div.x;
 }
