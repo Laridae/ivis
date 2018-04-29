9e85237cf6edea8a12abc879ac6470e532a51a7a
Laridae <christoffer14@gmail.com>
2018-04-29 14:48:12 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -3,9 +3,9 @@ alert("Du har virus!")
 var tick = 1
 
 function show_next_question(d) {
- var currentQ = document.getElementById("question" + d);
- var nextQ = document.getElementById("fråga" + (d+1));
- 
+ var currentQ = document.getElementById("question" + d).setAttribute(//"visible" = false);
+ var nextQ = document.getElementById("fråga" + (d+1)).setAttribute(//"visible" = true);
+ currentQ set attribute
  hide div.d; 
  show div.x;
 }
