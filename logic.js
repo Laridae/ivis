051bcb435b636eae123617a138725b2f7009d0a0
Laridae <christoffer14@gmail.com>
2018-04-29 15:00:53 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,6 +2,8 @@ function start_question(){
  //tick
 }
 
+document.getElementById("question" + (d+1)).setAttribute("class", "question active"); //next
+
 function show_next_question(d) {
  document.getElementById("question" + d).setAttribute("class", "question inactive");   //current
  document.getElementById("question" + (d+1)).setAttribute("class", "question active"); //next
