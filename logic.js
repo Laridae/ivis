62491a59d811cd72e7b87b5a0d868412ce4c2e83
Laridae <christoffer14@gmail.com>
2018-04-29 21:38:09 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -13,12 +13,13 @@ function show_next_question() {
  console.log(currentQ);
  document.getElementById("question" + currentQ).setAttribute("class", "question inactive");   //current
  document.getElementById("question" + (currentQ+1)).setAttribute("class", "question active"); //next
+ 
+ svg.select("circle:nth-child(" + currentQ + ")").style("fill", "green")
  currentQ = currentQ + 1;
  // We want to change group of [the node] to 'dark green'
  // Trick is that the id is currently "Klimatpolitik bör överordnas..." which can't be incremented.
  // TODO: remake so howering with cursor above text still shows text, but the id is a number.
  // document.getElementById("question" + currentQ).setAttribute("group", "5"); //remainder, "question" =/= node
  // svg.selectAll("circle").data().style("fill", "red") //Change the colour of currentNode to dark green. 
- svg.select("circle:nth-child(" + currentQ + ")").style("fill", "green")
  
 }
