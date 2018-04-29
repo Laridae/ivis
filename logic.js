ca71390affb97c225fcc7687ff7e7405c184cddc
Laridae <christoffer14@gmail.com>
2018-04-29 14:35:32 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ alert("Du har virus!")
 
 var tick = 1
 
-func show_next question ("siffran vi tar")
- x = siffran vi tar +1
- hide div x-1;
- show div x;
+func show_next question (d)
+ x = d + 1
+ hide div.d
+ show div.x
