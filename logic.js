a1628adabda15ffc8bc81594817f843227bb00eb
Laridae <christoffer14@gmail.com>
2018-04-29 14:53:46 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -3,8 +3,8 @@ alert("Du har virus!")
 var tick = 1
 
 function show_next_question(d) {
- var currentQ = document.getElementById("question" + d).setAttribute("class, question inactive");
- var nextQ = document.getElementById("fråga" + (d+1)).setAttribute("class, question active");
+ var currentQ = document.getElementById("question" + d).setAttribute("class", "question inactive");
+ var nextQ = document.getElementById("fråga" + (d+1)).setAttribute("class", "question active");
  currentQ set attribute
  hide div.d; 
  show div.x;
