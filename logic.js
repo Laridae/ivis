0f096daf06cc7d3b1c12ab439f275cd6ce0b8597
Laridae <christoffer14@gmail.com>
2018-04-30 12:16:55 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -20,6 +20,39 @@ function show_next_question() {
  
 }
 
+  function show_next_question2() {
+   console.log(currentQ);
+   document.getElementById("question" + currentQ).setAttribute("class", "question inactive");   //current
+   document.getElementById("question" + (currentQ+1)).setAttribute("class", "question active"); //next
+
+   svg.select("circle:nth-child(" + currentQ + ")").style("fill", "lime")
+   currentQ = currentQ + 1;
+   // If currentQ == 26 then{document.getElementById("options").setAttribute("class", "option inactive"); }  
+
+  }
+
+  function show_next_question3() {
+   console.log(currentQ);
+   document.getElementById("question" + currentQ).setAttribute("class", "question inactive");   //current
+   document.getElementById("question" + (currentQ+1)).setAttribute("class", "question active"); //next
+
+   svg.select("circle:nth-child(" + currentQ + ")").style("fill", "orange")
+   currentQ = currentQ + 1;
+   // If currentQ == 26 then{document.getElementById("options").setAttribute("class", "option inactive"); }  
+
+  }
+
+  function show_next_question4() {
+   console.log(currentQ);
+   document.getElementById("question" + currentQ).setAttribute("class", "question inactive");   //current
+   document.getElementById("question" + (currentQ+1)).setAttribute("class", "question active"); //next
+
+   svg.select("circle:nth-child(" + currentQ + ")").style("fill", "red")
+   currentQ = currentQ + 1;
+   // If currentQ == 26 then{document.getElementById("options").setAttribute("class", "option inactive"); }  
+
+  }
+
 function choose_3_important(d) {
  // Show 25 options (reveal a div in html)
  // Make a click increase the radius of 'd' by 2.5
